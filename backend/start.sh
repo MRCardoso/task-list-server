44418dbad978f2069c4aa28f292c886981265a74
marlonrcardoso <marlonrcardoso@yahoo.com.br>
2020-07-11 15:02:19 -0300

    fix update in User model to extends class of mine package

--- backend/start.sh
+++ backend/start.sh
@@ -21,15 +21,18 @@ sed -i "" -e "s/DB_USER/$dbuser/" .env
 sed -i "" -e "s/DB_PASS/$dbuser/" .env
 sed -i "" -e "s/DB_MIGRATION/migrations/" .env
 #change AWS configurations
-sed -i "" -e "s/BUCKET_NAME/task-list-development/" .env
-sed -i "" -e "s/BUCKET_PUBLIC/AKIAVU52HV6F7QVKYSEZ/" .env
-sed -i "" -e "s/BUCKET_SECRET/fhx3XJYOas7QZSdA\/41CRh+IuwOGrZ08RJNW8BHR/" .env
+sed -i "" -e "s/BUCKET_NAME/$AWS_TL_BUCKET/" .env
+sed -i "" -e "s/BUCKET_PUBLIC/$AWS_TL_PUBLIC/" .env
+sed -i "" -e "s/BUCKET_SECRET/$AWS_TL_SECRET/" .env
 sed -i "" -e "s/BUCKET_FOLDER/images/" .env
-sed -i "" -e "s/BUCKET_URL/s3\.amazonaws\.com/" .env
+sed -i "" -e "s/BUCKET_URL/$S3_URL/" .env
+## mail credentials
+sed -i "" -e "s/EMAIL_ADM/$MAIL_TL_SENDER/" .env
+sed -i "" -e "s/EMAIL_API/$MAIL_TL_KEY/" .env
 
 #install dependencies
 npm install
-npm install -g knex
 #run migrations
 knex migrate:latest
+#run seeds
 knex seed:run
